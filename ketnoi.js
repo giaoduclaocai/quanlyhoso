8eb3a69af2bbbd94270754a26cb75fbde5879e49
phanmemquanli <tulieuhopthanh@gmail.com>
2026-06-01 11:15:26 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -16,5 +16,5 @@ const KETNOI = {
     WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxJEriNwHp3twwlVk_yqViktUo96XOj71ciMdNsNY8TSuH_76t7gPt48DGnWU34gWVdeQ/exec",
 
     // 4. Mã định danh ứng dụng Google (OAuth 2.0 Client ID)
-    CLIENT_ID: "407480994586-m6fpq6sfcc90qqj9k08rsmi1lge6br94.apps.googleusercontent.com"
+    CLIENT_ID: "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com"
 };
